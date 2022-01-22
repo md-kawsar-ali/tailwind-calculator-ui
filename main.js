5238c6fa3e29cd2f95f64baab79ff5ef5effa72b
mdkawsarali <facebookerjordan@gmail.com>
2022-01-22 20:29:44 +0600

    Bugs Fixed

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-//Wrap code in an IIFE
 (function(){
   
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
+    let special = document.querySelectorAll('.btn-special');
     
-    //retrieve data from numbers that are clicked
+    //retrieve data from numbers and "+" "-" that are clicked
     buttons.forEach(function(button){
       button.addEventListener('click', function(e){
         let value = e.target.dataset.num;
         screen.value += value;
       })
     });
+
+    //retrieve data from "*" "/" that are clicked
+    special.forEach(function(button){
+      button.addEventListener('click', function(e){
+        if(screen.value !== ''){
+          let value = e.target.dataset.num;
+          screen.value += value;
+        } else {
+          screen.placeholder = 'Please Enter a Value';
+        }
+      })
+    });
     
     equal.addEventListener('click', function(e){
       if(screen.value === ''){
-        screen.value = 'Please Enter a Value';
+        screen.placeholder = 'Please Enter a Value';
       } else {
         let answer = eval(screen.value);
         screen.value = answer;
@@ -24,7 +36,10 @@
     })
     
     clear.addEventListener('click', function(e){
-      screen.value = '';
+      if(screen.value !== ''){
+        screen.value = '';
+        screen.placeholder = '';
+      }
     })
    
-  })(); //end IIFE
+  })();
